1c0fa264fba766a3bc598628e9944871fb80a153
sumeyyadede <dedesumeyya@gmail.com>
2020-06-27 20:09:26 -0400

    added TennisCoach class.
    added @Component for create bean.

---
+++ spring-demo-annotations/src/com/sumeyyadede/springdemo/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.sumeyyadede.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component("thatSillyCoach")
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+}
