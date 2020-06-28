db4c02d7dda1a4150ef4e537e77a656b44aaff25
sumeyyadede <dedesumeyya@gmail.com>
2020-06-27 21:15:11 -0400

    added getDailyFortune method.
    added FortuneService field.
    
    added constructor which is used to constructor injection.
    
    added @Autowired tag to constructor.

--- spring-demo-annotations/src/com/sumeyyadede/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/sumeyyadede/springdemo/TennisCoach.java
@@ -1,13 +1,27 @@
 package com.sumeyyadede.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	// @Autowired => Spring will scan for a component that implements FortuneService interface. For example: HappyFortuneService 
+	@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
