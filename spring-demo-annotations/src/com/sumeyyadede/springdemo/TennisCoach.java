cffccc5ffa99d356d9ccf3e556d6bfd99c214bb2
sumeyyadede <dedesumeyya@gmail.com>
2020-06-27 21:55:47 -0400

    added @Autowired annotation to FortuneService field.

--- spring-demo-annotations/src/com/sumeyyadede/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/sumeyyadede/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
 	private FortuneService fortuneService;
 	
 	// define a default constructor
@@ -13,12 +14,14 @@ public class TennisCoach implements Coach {
 		System.out.println("TennisCoach: inside default constructor");
 	}
 	
+	/*
 	// define the method
 	@Autowired
 	public void doSomeCrazyStuff(FortuneService fortuneService) {
 		System.out.println("TennisCoach: inside doSomeCrazyStuff() method");
 		this.fortuneService = fortuneService;
 	}
+	*/
 	
 	/*
 	// define a setter method
