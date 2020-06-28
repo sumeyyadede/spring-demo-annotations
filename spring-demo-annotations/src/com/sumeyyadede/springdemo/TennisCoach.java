4e4c50b06baea5e0120322c7fe86c53052841e07
sumeyyadede <dedesumeyya@gmail.com>
2020-06-27 22:16:08 -0400

    added @Qualifier annotation to be specific for dependency injection.

--- spring-demo-annotations/src/com/sumeyyadede/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/sumeyyadede/springdemo/TennisCoach.java
@@ -1,12 +1,15 @@
 package com.sumeyyadede.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+	// added @Qualifier annotation to be specific
 	@Autowired
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 	
 	// define a default constructor
