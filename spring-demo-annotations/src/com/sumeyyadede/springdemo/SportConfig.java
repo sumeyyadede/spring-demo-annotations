718c9f69b0face582d657922502ba232987cf45d
sumeyyadede <dedesumeyya@gmail.com>
2020-06-28 21:17:02 -0400

    added @Configuration annotation to the class.
    added bean for sadFortuneService and swimCoach.
    
    in swimCoach bean injected sadFortuneService dependency.

--- spring-demo-annotations/src/com/sumeyyadede/springdemo/SportConfig.java
+++ spring-demo-annotations/src/com/sumeyyadede/springdemo/SportConfig.java
@@ -1,10 +1,23 @@
 package com.sumeyyadede.springdemo;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;  
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.sumeyyadede.springdemo")
+//@ComponentScan("com.sumeyyadede.springdemo")
 public class SportConfig {
+	
+	// define bean for our sad fortune service
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	// define bean for our swim coach and inject dependency
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
 
 }
