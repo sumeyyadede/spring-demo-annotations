e83559434d6a40638fe0818f0836e991d92f08d9
sumeyyadede <dedesumeyya@gmail.com>
2020-06-28 21:59:28 -0400

    added @PropertSource annotation to make Java-based configuration.

--- spring-demo-annotations/src/com/sumeyyadede/springdemo/SportConfig.java
+++ spring-demo-annotations/src/com/sumeyyadede/springdemo/SportConfig.java
@@ -3,9 +3,11 @@ package com.sumeyyadede.springdemo;
 import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.ComponentScan;  
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //@ComponentScan("com.sumeyyadede.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 	
 	// define bean for our sad fortune service
